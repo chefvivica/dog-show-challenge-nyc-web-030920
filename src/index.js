c6c43984e7928cda38293ac117e5c9ff96bce7e1
Vivica <Ma>
2020-04-29 15:01:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -9,20 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
         return fetch(base)
         .then(r => r.json())
     }
-    
-    function getSingleDog(id){
-        return fetch("${base}"/"${id}")
-        .then(r => r.json())
-    }
 
     function addDog(dog){
-       const tr=  document.createElement('tr')
+       const tr = document.createElement('tr')
+       tr.dataset.id = dog.id
        tr.innerHTML = `
        <tr>
         <td>${dog.name}</td>
         <td>${dog.breed}</td> 
         <td>${dog.sex}</td> 
-        <td><button id="hi" data-id= ${dog.id}>Edit</button></td>
+        <td><button class="edit"}>Edit</button></td>
         </tr> 
        `
        return tr
@@ -33,55 +29,45 @@ document.addEventListener('DOMContentLoaded', () => {
             dogs.forEach(function(dog){
                 const tr = addDog(dog)
                 tableBody.append(tr)         
+                    
+                tableBody.addEventListener('click', function(event){
+                    if(event.target.className === 'edit'){
+                        const tr = event.target.parentNode.parentNode
+                        const cells = tr.getElementsByTagName("td")
+                        console.log(cells)
+                        const name = cells[0].textContent
+                        const breed = cells[1].textContent
+                        const sex = cells[2].textContent
+                        
+                        form.name.value = name
+                        form.breed.value = breed
+                        form.sex.value = sex
+                        form.dataset.id = tr.dataset.id
+                        
+                        document.addEventListener("submit", function(e){
+                            
+                            e.preventDefault()
+                    
+                            const form = e.target
+                            const id = form.dataset.id
+                            const name = form.name.value
+                            const breed = form.breed.value
+                            const sex = form.sex.value
+                         
+                    
+                            fetch(`${base}/${id}`,{
+                                method :"PATCH",
+                                headers:{"accept":"application/json",
+                                        "content-type":"application/json"
+                                        },
+                                body: JSON.stringify({name, breed, sex})
+                            
+                            })
+
+                        })
+                    }
+                })
             })
-        })
-    }
-    
-
-
-    form.addEventListener("submit", createADog)
-
-    function createADog(e){
-        e.preventDefault()
-        const form = e.target
-        const name = form.name.value
-        const breed = form.breed.value
-        const sex = form.sex.value
-        const newDog = {name, breed, sex}      
-        
-        console.log(newDog)
-        fetch(base,{
-            method :"POST",
-            headers:{"accept":"application/json",
-                    "content-type":"application/json"
-                    },
-            body: JSON.stringify(newDog)
-        })
-
-    }
-    // const btns = tableBody.querySelectorAll("#hi")
-    // console.log(btns)
-    // btns.forEach(function(btn){
-    //     btn.addEventListener("click",function(event){
-    //         getSingleDog(id).then(dog=>{
-    //             const btn = e.target
-    //             const id = btn.dataset.id
-    //             const name = dog.name
-    //             const breed = dog.breed
-    //             const sex = dog.sex 
-    //             const newDog = {name , breed, sex}
-    //         updateAdog(id,newDog)
-    //     })
-    // })
-
-      
-    function updateAdog(id, newDog){ 
-        fetch("${base}"/"${id}",{
-            method :"PATCH",
-            headers:{"accept":"application/json",
-                    "content-type":"application/json"
-                    },
-            body: JSON.stringify(id, newDog)
         })
     }
     
