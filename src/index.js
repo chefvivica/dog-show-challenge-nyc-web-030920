b4b5d09408ee7c526ed796c1b063e9937264fe29
Vivica <Ma>
2020-05-20 10:30:54 -0400

    v

--- src/index.js
+++ src/index.js
@@ -1,74 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const base = "http://localhost:3000/dogs"
-    const tableBody = document.getElementById("table-body")
-    const form = document.getElementById("dog-form")
-    
-    showDogs()
+// √get dogs and render on the table
+//√<tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>
 
-    function getDogs(){
-        return fetch(base)
-        .then(r => r.json())
-    }
+// add el to edit btn, be able to grab the form's value and PATCH the info to database
+//http://localhost:3000/dogs/:id (name,breed, sex)
 
-    function addDog(dog){
-       const tr = document.createElement('tr')
-       tr.dataset.id = dog.id
-       tr.innerHTML = `
-       <tr>
-        <td>${dog.name}</td>
-        <td>${dog.breed}</td> 
+//re send get fetch and change the dom without refresh ( clear out the innerHTML)
+
+document.addEventListener("DOMContentLoaded",()=>{
+  const base = ' http://localhost:3000/dogs'
+  const tBody = document.getElementById('table-body')
+
+
+  const getDogs = () =>{
+    fetch(base)
+    .then(r=>r.json())
+    .then(renderDogs)
+  }
+
+
+  const renderDogs = dogs =>{
+    dogs.forEach(dog=>{
+      const tr = document.createElement('tr')
+      tr.innerHTML = `
+      <td>${dog.name}</td>
+      <td>${dog.breed}</td>
         <td>${dog.sex}</td> 
-        <td><button class="edit"}>Edit</button></td>
-        </tr> 
-       `
-       return tr
-    }
+        <td><button class="edit">Edit</button></td> 
+      `
+      tBody.append(tr)
+    })
+  }
 
-    function showDogs(){
-        getDogs().then(dogs => {
-            dogs.forEach(function(dog){
-                const tr = addDog(dog)
-                tableBody.append(tr)         
-                    
-                tableBody.addEventListener('click', function(event){
-                    if(event.target.className === 'edit'){
-                        const tr = event.target.parentNode.parentNode
-                        const cells = tr.getElementsByTagName("td")
-                        console.log(cells)
-                        const name = cells[0].textContent
-                        const breed = cells[1].textContent
-                        const sex = cells[2].textContent
-                        
-                        form.name.value = name
-                        form.breed.value = breed
-                        form.sex.value = sex
-                        form.dataset.id = tr.dataset.id
-                        
-                        document.addEventListener("submit", function(e){
-                            
-                            e.preventDefault()
-                    
-                            const form = e.target
-                            const id = form.dataset.id
-                            const name = form.name.value
-                            const breed = form.breed.value
-                            const sex = form.sex.value
-                         
-                    
-                            fetch(`${base}/${id}`,{
-                                method :"PATCH",
-                                headers:{"accept":"application/json",
-                                        "content-type":"application/json"
-                                        },
-                                body: JSON.stringify({name, breed, sex})
-                            
-                            })
-
-                        })
-                    }
-                })
-            })
-        })
-    }
+  document.addEventListener('submit', e=>{
+    e.preventDefault()
+      const form = document.querySelector('#dog-form')
+      const name = form.name.value 
+      const breed = form.breed.value
+      const sex = form.sex.value
+      const newDog = {name, breed, sex}
+     
     
+    
+  })
+
+  getDogs()
+
 })
