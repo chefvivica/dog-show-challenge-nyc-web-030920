6b458a9917de0aa9c21ad9aa47bb4e8cf5bf565e
Vivica <Ma>
2020-05-20 11:06:39 -0400

    vivi

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // √get dogs and render on the table
 //√<tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>
 
-// add el to edit btn, be able to grab the form's value and PATCH the info to database
-//http://localhost:3000/dogs/:id (name,breed, sex)
+// √add el to edit btn, be able to grab the form's value and PATCH the info to database
+//√http://localhost:3000/dogs/:id (name,breed, sex)
 
 //re send get fetch and change the dom without refresh ( clear out the innerHTML)
 
@@ -25,23 +25,51 @@ document.addEventListener("DOMContentLoaded",()=>{
       <td>${dog.name}</td>
       <td>${dog.breed}</td>
         <td>${dog.sex}</td> 
-        <td><button class="edit">Edit</button></td> 
+        <td><button class="edit" data-id=${dog.id}>Edit</button></td> 
       `
       tBody.append(tr)
+      const btn = document.querySelector('.edit')
+      btn.addEventListener("click",showDogInfo)
     })
   }
 
-  document.addEventListener('submit', e=>{
-    e.preventDefault()
-      const form = document.querySelector('#dog-form')
+
+  const form = document.querySelector('#dog-form')
+  const showDogInfo =(e)=>{
+    const tr = e.target.parentElement.parentElement
+    const name = tr.children[0].innerText
+    const breed = tr.children[1].innerText
+    const sex =  tr.children[2].innerText
+
+    form.name.value = name
+    form.breed.value = breed
+    form.sex.value = sex
+
+    const id = e.target.dataset.id 
+
+    newDogInfo(id)
+  }
+
+  const newDogInfo = (id) =>{
+    document.addEventListener('submit', e=>{
+      e.preventDefault()
+    
       const name = form.name.value 
       const breed = form.breed.value
-      const sex = form.sex.value
+      const sex= form.sex.value
       const newDog = {name, breed, sex}
-     
+      
+      fetch(`http://localhost:3000/dogs/${id}`,{
+        method: "PATCH",
+        headers:{"accept":"application/json",
+                "content-type":"application/json"
+                },
+        body:JSON.stringify(newDog)
+      })
     
+    })
     
-  })
+  }
 
   getDogs()
 
